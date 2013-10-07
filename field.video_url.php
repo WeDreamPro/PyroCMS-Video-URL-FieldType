f22f71ce2ec1c11205a37af11c5d9e780c868d92
jose fonseca <jose@ditecnologia.com>
2013-10-07 10:45:26 -0500

    resolved #4

--- field.video_url.php
+++ field.video_url.php
@@ -16,8 +16,8 @@ class Field_video_url {
 
     public $field_type_slug = 'video_url';
     public $db_col_type = 'text';
-    public $version = '1.1.0';
-    public $custom_parameters = array('video_heigth','video_width','video_autoplay');
+    public $version = '1.2.1';
+    public $custom_parameters = array('video_heigth', 'video_width', 'video_autoplay');
     public $author = array('name' => 'Rigo B Castro', 'url' => 'http://rigobcastro.com');
 
     // --------------------------------------------------------------------------
@@ -29,7 +29,8 @@ class Field_video_url {
      * @param   $params	array
      * @return	string
      */
-    public function form_output($params) {
+    public function form_output($params)
+    {
         $video_info = !empty($params['value']) ? json_decode($params['value']) : null;
 
         $input_options = array(
@@ -37,6 +38,9 @@ class Field_video_url {
             'type' => 'text',
             'id' => $params['form_slug'],
             'data-fieldtype' => 'video_url',
+            'data-video-width' => !empty($params['video_width']) ? $params['video_width'] : '100%',
+            'data-video-width' => !empty($params['video_width']) ? $params['video_width'] : '100%',
+            'data-video-autoplay' => !empty($params['video_autoplay']) ? $params['video_autoplay'] : 0,
             'value' => !empty($video_info->url) ? $video_info->url : null,
             'placeholder' => lang('streams:video_url.input_placeholder')
         );
@@ -46,39 +50,41 @@ class Field_video_url {
         );
 
         return $this->CI->type->load_view('video_url', 'input', array(
-                    'input_options' => $input_options,
-                    'input_hidden_options' => $input_hidden_options
-                ));
+                'input_options' => $input_options,
+                'input_hidden_options' => $input_hidden_options
+        ));
     }
-    
-    // --------------------------------------------------------------------------
-    
+
+    // ----------------------------------------------------------------------
+
     /**
      * Custom parameters
      * @author Jose Fonseca <jose@ditecnologia.com>
      */
-
-    public function param_video_heigth($value = null) {
+    public function param_video_heigth($value = null)
+    {
         return array(
             'input' => form_input('video_heigth', $value),
             'instructions' => $this->CI->lang->line('streams.video_heigth.instructions')
         );
     }
-    
-    public function param_video_width($value = null) {
+
+    public function param_video_width($value = null)
+    {
         return array(
             'input' => form_input('video_width', $value),
             'instructions' => $this->CI->lang->line('streams.video_width.instructions')
         );
     }
-    
-    public function param_video_autoplay($value = null) {
+
+    public function param_video_autoplay($value = null)
+    {
         $options = array(
             '0' => $this->CI->lang->line('streams.video_width.no'),
             '1' => $this->CI->lang->line('streams.video_width.yes')
         );
         return array(
-            'input' => form_dropdown('video_autoplay',$options, $value),
+            'input' => form_dropdown('video_autoplay', $options, $value),
             'instructions' => $this->CI->lang->line('streams.video_autoplay.instructions')
         );
     }
@@ -94,14 +100,16 @@ class Field_video_url {
      * @param	array
      * @return	array
      */
-    public function pre_output_plugin($input,$params) {
+    public function pre_output($input, $params)
+    {
         if (!$input)
             return null;
         $data = json_decode($input);
+        
         /** define defaults **/
         $height = 315;
         $width = 560;
-        $source = $data->src;
+        $source = $data->provider_name == 'Vimeo' ? "//player.vimeo.com/video/".$data->video_id : "//youtube.com/embed/".$data->video_id ;
         /** set options **/
         if(!empty($params['video_heigth'])){
             $height = $params['video_heigth'];
@@ -113,7 +121,37 @@ class Field_video_url {
             $source = $source.'?autoplay=1';
         }
         $iframe = '<iframe width="'.$width.'" src="' . $source . '" height="'.$height.'" frameborder="0" allowfullscreen></iframe>';
-        return $iframe;
+        $data->html = $iframe;
+        
+        
+        return $data;
+    }
+
+    // ----------------------------------------------------------------------
+
+    public function pre_output_plugin($input,$params)
+    {
+        if (!$input)
+            return null;
+        $data = json_decode($input);
+        
+        /** define defaults **/
+        $height = 315;
+        $width = 560;
+        $source = $data->provider_name == 'Vimeo' ? "//player.vimeo.com/video/".$data->video_id : "//youtube.com/embed/".$data->video_id ;
+        /** set options **/
+        if(!empty($params['video_heigth'])){
+            $height = $params['video_heigth'];
+        }
+        if(!empty($params['video_width'])){
+            $width = $params['video_width'];
+        }
+        if(!empty($params['video_autoplay'])){
+            $source = $source.'?autoplay=1';
+        }
+        $iframe = '<iframe width="'.$width.'" src="' . $source . '" height="'.$height.'" frameborder="0" allowfullscreen></iframe>';
+        $data->html = $iframe;
+        return (array) $data;
     }
 
     // ----------------------------------------------------------------------
@@ -127,7 +165,8 @@ class Field_video_url {
      * @param $field object
      * @return void
      */
-    public function event($field) {
+    public function event()
+    {
         $this->CI->type->add_js('video_url', 'video_url.js');
     }
 
--- js/video_url.js
+++ js/video_url.js
@@ -1,76 +1,100 @@
-(function($){
-	var inputs = '[data-fieldtype="video_url"]';
-	var provider = {'name': 'youtube'};
-	function get_youtube_from_url(){
-		var $this = $(this),
-		match = $(this).val().match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?.*v=|\/)([^\s&]+)/),
-		$preview_content = $(this).parent().find('.preview_video_url');
-		if(match !== null){
-			$.getJSON('http://gdata.youtube.com/feeds/api/videos/'+match[1]+'?v=2&alt=jsonc',function(data){
-				provider.name = "youtube";
-				return render_preview.call($this, data.data.id);
-			});
-		} else {
-			$preview_content.hide();
-		}
-	}
+(function($) {
+    var inputs = '[data-fieldtype="video_url"]',
+        youtube_regex = /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?.*v=|\/)([^\s&]+)/,
+        vimeo_regex = /(?:https?:\/{2})?(?:w{3}\.)?vim(?:eo)?\.(?:com)([^\s&]+)/;
 
-	function get_vimeo_from_url(){
-		var $this = $(this),
-		match = $(this).val().match(/(?:https?:\/{2})?(?:w{3}\.)?vim(?:eo)?\.(?:com)([^\s&]+)/),
-		$preview_content = $(this).parent().find('.preview_video_url');
-		if(match !== null){
-			provider.name = "vimeo";
-			return render_preview.call($this, match[1]);
-		} else {
-			$preview_content.hide();
-		}
-	}
+    function get_video_from_url() {
+        var $this = $(this),
+            url = $this.val(),
+            service_url = null,
+            match = url.match(youtube_regex),
+            $preview_content = $(this).parent().find('.preview_video_url'),
+            video_id_tosave = 0;
+            
+        if (match !== null) {
+            video_id_tosave = match[1];
+            // service_url = 'http://gdata.youtube.com/feeds/api/videos/' + match[1] + '?v=2&alt=jsonc';
+            service_url = 'http://www.youtube.com/oembed?url=' + encodeURIComponent(url);
+        } else {
+            match = url.match(vimeo_regex);
 
-	function get_video_from_url(){
-		var $this = $(this);
-		service = $this.val().match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?.*v=|\/)([^\s&]+)/);
-		if(service === null){
-			get_vimeo_from_url.call($this);
-		}else{
-			get_youtube_from_url.call($this);
-		}
-	}
+            if (match !== null) {
+                // service_url = 'http://vimeo.com/api/v2/video'+match[1]+'.json';
+                service_url = 'http://vimeo.com/api/oembed.json?url=' + encodeURIComponent(url);
+            }
+        }
 
-	function render_preview(id){
-		var $preview_content = $(this).parent().find('.preview_video_url'),
-		$iframe = $preview_content.find('iframe'),
-		$hidden = $preview_content.find('[type="hidden"]');
-		if(provider.name === "youtube"){
-			var src = $iframe.data('youtube-src') +  id;
-		}else{
-			var src = $iframe.data('vimeo-src') +  id;
-		}
+        if (service_url !== null) {
+            $.ajax({
+                url: 'http://query.yahooapis.com/v1/public/yql',
+                data: {
+                    q: "select * from json where url ='" + service_url + "'",
+                    format: "json"
+                },
+                dataType: "jsonp",
+                beforeSend: function() {
+                    $preview_content.find('.iframe-preview').empty();
+                },
+                success: function(yql_result) {
+                    var data = yql_result.query.results.json;
+                    if(video_id_tosave != 0 ){
+                        data.video_id = video_id_tosave;
+                    }
+                    if (data) {
+                        render_preview.call($this, data);
+                    }
+                },
+                error: function(result) {
+                    alert("Sorry no data found.");
+                }
+            });
+        }
+    }
 
-		$iframe.attr('src', src);
+    function render_preview(data) {
+        var $preview_content = $(this).parent().find('.preview_video_url'),
+            $iframe_content = $preview_content.find('.iframe-preview'),
+            $hidden = $preview_content.find('[type="hidden"]');
 
-		$hidden.val(JSON.stringify({
-			url : $(this).val(),
-			src : src
-		}));
 
-		return $preview_content.show();
-	}
 
-	$(document).on('keyup', inputs, get_video_from_url);
+        if (data.html) {
+            var $video = $(data.html),
+                video_html = $video.clone().attr('width', 500).attr('height', 300);
 
-	$(document).on('click', inputs, function(){
-		$(this).select();
-	});
+            $iframe_content.html(video_html);
 
-	$(document).on('click', '.show_video_url', function(e){
-		$.colorbox({ href: this.href, width: '600px', height: '400px', iframe: true });
-		return e.preventDefault();
-	});
+            $video.attr('width', $(this).data('video-width')).attr('height', $(this).data('video-height'));
+            
+            if($(this).data('video-autoplay')){
+                
+            }
+        }
 
-	$(function(){
-		$.each($(inputs), function(){
-			get_video_from_url.call(this);
-		});
-	});
+        data = $.extend(data, {
+            url: $(this).val(),
+            html: $video[0].outerHTML
+        });
+
+        $hidden.val(JSON.stringify(data));
+
+        return $preview_content.show();
+    }
+
+    $(document).on('keyup', inputs, get_video_from_url);
+
+    $(document).on('click', inputs, function() {
+        $(this).select();
+    });
+
+    $(document).on('click', '.show_video_url', function(e) {
+        $.colorbox({href: this.href, width: '600px', height: '400px', iframe: true});
+        return e.preventDefault();
+    });
+
+    $(function() {
+        $.each($(inputs), function() {
+            get_video_from_url.call(this);
+        });
+    });
 })(window.jQuery);
