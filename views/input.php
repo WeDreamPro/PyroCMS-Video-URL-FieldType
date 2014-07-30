850a1d4ac3752cb1fa8c172d6bb6418e0eadd2c1
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-07-30 12:02:54 -0500

    Fix front end render, add JS in case the URI is not admin so the field work in front end forms

--- views/input.php
+++ views/input.php
@@ -1,5 +1,9 @@
 <?php echo form_input($input_options) ?>
 
+<?php if($this->uri->segment(1) !== 'admin'): ?>
+	<script type="text/javascript" src="<?=base_url('streams_core/field_asset/js/video_url/video_url.js');?>"></script>
+<?php endif;?>
+
 <div class="preview_video_url" style="display: none;">
     <div class="iframe-preview"></div>
     <?php echo form_hidden($input_hidden_options) ?>
